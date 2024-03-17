8b406bb74aabb2e6dcd030804c3f600645bc21ba
Ashurkovav <AshurkovAV@yandex.ru>
2024-03-17 14:43:22 +0300

    +add logger

---
+++ JobTaxi.Entity/Log/FileLoggerProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace JobTaxi.Entity.Log
+{
+    public class FileLoggerProvider : ILoggerProvider
+    {
+        private string path;
+        public FileLoggerProvider(string _path)
+        {
+            path = _path;
+        }
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new FileLogger(path);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
